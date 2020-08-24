1497dbc6e47ecb57c0ac48299aa539264bcb9d7d
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-24 11:32:44 -0700

    remove TDD expectations from README.md and .js, re-clarify Specs per Travis, move alert to UI
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>

--- README.md
+++ README.md
@@ -9,24 +9,13 @@
 _This HTML demonstrates the fundamental understanding of creating objects in JavaScript and building out business logic with specs by hiding/showing user data regarding travel locations in clickable lists. 
 
 ## Specs
-| Spec | Description | Input | Output |
-| :-------------     | :------------- | :------------- |:------------- |
-| 0 | **Homepage** | User accesses localhost:5004 | Homepage with user input form |
-| 1 |**The program will collect user travel data.** | User input: "Country" "State" "City" "Landmark" "Dates" "Notes"| Output: "USA" "Washington" "Seattle" "Space Needle" "June 2019" "Notes: Went with Mom."|
-| 2 |**The program will display multiple places.** | Page Display: "Washington, USA" |  |
-| 3 |**The program will allow user to click each place to reveal loaction data.** | User input: "Washington, USA" | Page Display: "Seattle, Space Needle, June 2019, Notes: Went with Mom." |
-
-## Test Driven Development:
-* Describe: places()
-
-* Test 1: "Return alert for enter data"
-* _Expect: (places("USA")).toEqual("Whoops!...missing data");_
-
-* Test 2: "Return multiple clickable destinations from user input."_
-* _Expect: (places("Washington, Utah")).toEqual("Washington, USA" "Utah, USA");
-
-* Test 3: "Clickable destionations retrun a list of user data".
-* _Expect: (places("Washington, USA")).toEqual("USA" "Washington" "Seattle" "Space Needle" "June 2019" "Notes: Went with Mom.");_
+// ## Specs
+// | Spec | Description | Input | Output |
+// | :-------------     | :------------- | :------------- |:------------- |
+// | 1 | **User can create Places object | UI: var place1 = new Places(); typeof(place1) | output: "object" ex: place1.country = "USA" |
+// | 2 |**The program will display multiple places.** | Page Display: "Washington, USA" |  |
+// | 3 |**The program will allow user to click each place to reveal loaction data.** | User input: "Washington, USA" | Page Display: "Seattle, Space Needle, June 2019, Notes: Went with Mom." |
+// | 4 | **Homepage** | User accesses localhost:5004 | Homepage with user input form |
 
 ## Setup/Installation Requirements
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,33 @@
-// ## Test Driven Development:
-// * Describe: places()
-
-// * Test 1: "Return alert for enter data"
-// * _Expect: (places("USA")).toEqual("Whoops!...missing data");_
-
-// * Test 2: "Return multiple clickable destinations from user input."_
-// * _Expect: (places("Washington, Utah")).toEqual("Washington, USA" "Utah, USA");
-
-// * Test 3: "Clickable destionations retrun a list of user data".
-// * _Expect: (places("Washington, USA")).toEqual("USA" "Washington" "Seattle" "Space Needle" "June 2019" "Notes: Went with Mom.");_
-
 // Business Interface
 
 function Places() {
     this.places = [];
-    if (this.places = undefined){
-      alert("Whoops! You forgot to enter something. Try again!");
-    } else {
-      return ("success!");
-    }
+
+function Places(country, stateName, cityName, landmark, dateRange, notes) {
+    this.country = country;
+    this.stateName = stateName;
+    this.cityName = cityName;
+    this.landmark = landmark;
+    this.dateRange = dateRange;
+    this.notes = notes;
 }
-
-// function Places(country, stateName, cityName, landmark, dateRange, notes) {
-//     this.country = country;
-//     this.stateName = stateName;
-//     this.cityName = cityName;
-//     this.landmark = landmark;
-//     this.dateRange = dateRange;
-//     this.notes = notes;
-// }
-
-
-
 let place1 = new Places("USA", "Washington", "Seattle", "Space Needle", "2012", "With Friends");
 
 // User Interface
 
+// Alert moved from Business Interface
+// if (this.places.length(); === 0){
+//   console.log("Whoops! You forgot to enter something. Try again!");
+// } else {
+//   console.log("success!");
+// }
+// }
 
 
 // ## Specs
 // | Spec | Description | Input | Output |
 // | :-------------     | :------------- | :------------- |:------------- |
-// | 0 | **Homepage** | User accesses localhost:5004 | Homepage with user input form |
-// | 1 |**The program will collect user travel data.** | User input: "Country" "State" "City" "Landmark" "Dates" "Notes"| Output: "USA" "Washington" "Seattle" "Space Needle" "June 2019" "Notes: Went with Mom."|
+// | 1 | **User can create Places object | UI: var place1 = new Places(); typeof(place1) | output: "object" ex: place1.country = "USA" |
 // | 2 |**The program will display multiple places.** | Page Display: "Washington, USA" |  |
 // | 3 |**The program will allow user to click each place to reveal loaction data.** | User input: "Washington, USA" | Page Display: "Seattle, Space Needle, June 2019, Notes: Went with Mom." |
+// | 4 | **Homepage** | User accesses localhost:5004 | Homepage with user input form |
