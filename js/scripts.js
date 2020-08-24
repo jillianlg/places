23e82c3fa1532abca7daed5aeb95dd73264945d5
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-24 12:05:46 -0700

    Test 3a-3f Unique ID FAILED, breaking for lunch
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,33 @@
-// Business Interface
+// Business Logic for Website
+function Travel() {
+  this.places = [];
+  this.placeId = 0;
+}
 
-function Places() {
-    this.places = [];
+  Travel.prototype.addPlace = function(place) {
+    place.id = this.assignId();
+    this.places.push(place);
+  }
 
-function Places(country, stateName, cityName, landmark, dateRange, notes) {
+  Travel.prototype.assignId = function() {
+    this.placeId += 1;
+    return this.placeId;
+  }
+  
+  Travel.prototype.findPlace = function(id) {
+    for (let i=0; i< this.places.length; i++) {
+      if (this.places[i]) {
+        if (this.place[i].id == id) {
+          return this.places[i];
+        }
+      }
+    };
+    return false;
+  }
+
+// Business Logic for Places
+
+function Place(country, stateName, cityName, landmark, dateRange, notes) {
     this.country = country;
     this.stateName = stateName;
     this.cityName = cityName;
@@ -11,7 +35,10 @@ function Places(country, stateName, cityName, landmark, dateRange, notes) {
     this.dateRange = dateRange;
     this.notes = notes;
 }
-let place1 = new Places("USA", "Washington", "Seattle", "Space Needle", "2012", "With Friends");
+// let city1 = new Place("USA", "Washington", "Seattle", "Space Needle", "2012", "With Friends");
+// let city2 = new Place("USA", "Oregon", "Portland", "Mulnomah Falls", "2011", "With Family");
+// Travel.addPlace (city1);
+// Travel.addPlace (city2);
 
 // User Interface
 
@@ -23,11 +50,3 @@ let place1 = new Places("USA", "Washington", "Seattle", "Space Needle", "2012", 
 // }
 // }
 
-
-// ## Specs
-// | Spec | Description | Input | Output |
-// | :-------------     | :------------- | :------------- |:------------- |
-// | 1 | **User can create Places object | UI: var place1 = new Places(); typeof(place1) | output: "object" ex: place1.country = "USA" |
-// | 2 |**The program will display multiple places.** | Page Display: "Washington, USA" |  |
-// | 3 |**The program will allow user to click each place to reveal loaction data.** | User input: "Washington, USA" | Page Display: "Seattle, Space Needle, June 2019, Notes: Went with Mom." |
-// | 4 | **Homepage** | User accesses localhost:5004 | Homepage with user input form |
